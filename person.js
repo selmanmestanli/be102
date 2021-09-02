7789b28dd24415f1f9474d04bfea957db6e3cc98
selmanmestanli <selmanmestanli@gmail.com>
2021-09-02 11:40:10 +0200

    Add age*4 in person.js

--- person.js
+++ person.js
@@ -8,7 +8,7 @@ class Person {
   }
 
   greet(person) {
-    console.log(`Hello ${person.name}, this is ${this.name} at age ${this.age}*3`)
+    console.log(`Hello ${person.name}, this is ${this.name} at age ${this.age}*4`)
   }
 
   addPhoto(photo) {
